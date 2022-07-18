7eff6eef0375b763e2a7735c9dfe6fda85a58aac
TakahashiJumpei <b1014098@gmail.com>
2022-07-18 11:51:58 +0900

    ファビコンの実装

--- src/resources/views/layout/app.blade.php
+++ src/resources/views/layout/app.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title')</title>
 
+    <link rel="icon" href="favicon.ico">
+
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
